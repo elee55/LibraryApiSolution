48b390bde0b967da3719b04774c4a5beeb659dc2
Ellis Elee <EElee55@outlook.com>
2020-04-27 21:49:43 +0000

    First commit

---
+++ LibraryApi/Controllers/StatusController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class StatusController : Controller
+    {
+        // GET / status -> 200 OK
+        [HttpGet("/status")]
+        public ActionResult GetTheStatus()
+        {
+            var response = new GetStatusResponse
+            {
+                Message = "Everyhting is golden!",
+                CheckedBy = "Joe Schmidt",
+                WhenLastChecked = DateTime.Now
+            };
+            return Ok(response);
+        }  
+    }
+    public class GetStatusResponse
+    {
+        public string Message { get; set; }
+        public string CheckedBy { get; set; }
+        public DateTime WhenLastChecked { get; set; }
+    }
