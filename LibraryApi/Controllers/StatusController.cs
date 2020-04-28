5cc5cefc7057636968828946da3178409bfb294c
Ellis Elee <EElee55@outlook.com>
2020-04-28 21:56:18 +0000

    Tuesdau work

--- LibraryApi/Controllers/StatusController.cs
+++ LibraryApi/Controllers/StatusController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryApi.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,22 +9,70 @@ namespace LibraryApi.Controllers
 {
     public class StatusController : Controller
     {
+        ISystemTime systemTime;
+        public StatusController(ISystemTime systemTime)
+        {
+            this.systemTime = systemTime;
+        }
+
+
         // GET / status -> 200 OK
         [HttpGet("/status")]
         public ActionResult GetTheStatus()
         {
             var response = new GetStatusResponse
             {
-                Message = "Everyhting is golden!",
+                Message = "Everything is golden!",
                 CheckedBy = "Joe Schmidt",
                 WhenLastChecked = DateTime.Now
             };
             return Ok(response);
-        }  
-    }
-    public class GetStatusResponse
+        }
+
+        // GET /employees/93/salary
+        [HttpGet("employees/{employeeId:int:min(1)}/salary")]
+        public ActionResult GetEmployeeSalary(int employeeId)
+        {
+            return Ok($"The Employee {employeeId} has a salary of $87,000.00");
+        }
+
+        [HttpGet("employees")]
+        public ActionResult GetEmployees([FromQuery]string dept = "All")
+        {
+            return Ok($"Returning employees for department {dept}");
+        }
+        // "Kinds of resources {Resource Archetypes}
+        // 1. Document - single thing - GET /employee/52
+        // 2. Collection - a plural thing
+        // 3. Store
+        // 4. Controller
+
+
+
+        [HttpGet("whoami")]
+        public ActionResult WhoAmI([FromHeader(Name = "User-Agent")] string userAgent)
+        {
+            return Ok($"I see you are running {userAgent}");
+        }
+
+        [HttpPost("employees")]
+        public ActionResult HireEmployee([FromBody] EmployeeCreateRequest employeeToHire)
+        {
+            return Ok($"Hiring {employeeToHire.LastName} as a {employeeToHire.Department}");
+        }
+}
+
+public class GetStatusResponse
     {
         public string Message { get; set; }
         public string CheckedBy { get; set; }
         public DateTime WhenLastChecked { get; set; }
     }
+
+    public class EmployeeCreateRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+}
--- LibraryApi/Startup.cs
+++ LibraryApi/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibraryApi.Domain;
+using LibraryApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +27,17 @@ namespace LibraryApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.IgnoreNullValues = true;
+                });
+
+            services.AddTransient<ISystemTime, SystemTime>();
+            
+            services.AddDbContext<LibraryDataContext>(options =>
+                options.UseSqlServer(@"server=.\sqlexpress;database=library;integrated security=true") // Need to fix it later
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
